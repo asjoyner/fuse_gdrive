4c455c4749486278fb0ce94d142d36893399043d
Aaron S. Joyner <aaron@joyner.ws>
2015-05-06 20:35:46 -0400

    Correctly export blocksize and num blocks per file

--- fuse_fs.go
+++ fuse_fs.go
@@ -24,6 +24,7 @@ import (
 
 // https://developers.google.com/drive/web/folder
 const driveFolderMimeType string = "application/vnd.google-apps.folder"
+const blockSize uint32 = 4096
 
 // serveConn holds the state about the fuse connection
 type serveConn struct {
@@ -76,7 +77,16 @@ func (sc *serveConn) serve(req fuse.Request) {
 		req.Respond(&resp)
 
 	case *fuse.StatfsRequest:
-		req.Respond(&fuse.StatfsResponse{})
+		var numfiles uint64
+		if f, err := sc.db.AllFileIds(); err != nil {
+			numfiles = uint64(len(f))
+		}
+		req.Respond(
+			&fuse.StatfsResponse{
+				Files: numfiles,
+				Bsize: blockSize,
+			},
+		)
 
 	case *fuse.GetattrRequest:
 		sc.getattr(req)
@@ -275,7 +285,7 @@ func (sc *serveConn) attrFromFile(file drive_db.File) fuse.Attr {
 		Gid:    sc.gid,
 		Mode:   0755,
 		Size:   uint64(file.FileSize),
-		Blocks: uint64(file.FileSize),
+		Blocks: uint64(file.FileSize / int64(blockSize)),
 	}
 	if file.MimeType == driveFolderMimeType {
 		attr.Mode = os.ModeDir | 0755
